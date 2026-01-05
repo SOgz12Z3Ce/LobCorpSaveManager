66045c7e5bc62e008e3af6011add2ac7246d0379
SOgz12Z3Ce <sogz12z3ce@gmail.com>
2026-01-05 13:14:32 +0800

    feat: add usage tips and exit code

--- LobCorpSaveSerializer/src/LobcorpSaveSerializer/LobCorp/Program.cs
+++ LobCorpSaveSerializer/src/LobcorpSaveSerializer/LobCorp/Program.cs
@@ -6,13 +6,19 @@ namespace LobCorp
 {
 	internal class Program
 	{
-		static void Main(string[] args)
+		static int Main(string[] args)
 		{
+			if (args.Length != 1)
+			{
+				Console.WriteLine("Usage is: lobcss file");
+				return 1;
+			}
 			using (FileStream fileStream = File.Open(args[0], FileMode.Open))
 			{
 				LobCorpSave lobCorpSave = LobCorpSaveSerializer.Deserialize(fileStream);
 				Console.WriteLine(lobCorpSave.ToJson(Newtonsoft.Json.Formatting.Indented));
 			}
+			return 0;
 		}
 	}
 }
