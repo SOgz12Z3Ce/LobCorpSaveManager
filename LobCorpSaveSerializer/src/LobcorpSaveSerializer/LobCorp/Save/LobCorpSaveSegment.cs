5fb9bdf26510bb80c4de9700e2724a9cfaac79dc
SOgz12Z3Ce <sogz12z3ce@gmail.com>
2026-01-06 21:53:38 +0800

    feat(LobCorpSaveSerializer): support serialize JSON to `etc170808.dat`

--- LobCorpSaveSerializer/src/LobcorpSaveSerializer/LobCorp/Save/LobCorpSaveSegment.cs
+++ LobCorpSaveSerializer/src/LobcorpSaveSerializer/LobCorp/Save/LobCorpSaveSegment.cs
@@ -1,8 +1,12 @@
 using System;
+using System.CodeDom;
 using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting;
 using LobCorp.Exceptions;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Serialization;
 
 namespace LobCorp.Save
 {
@@ -18,13 +22,21 @@ namespace LobCorp.Save
 			{ "money", SaveType.Unlimited },
 			{ "genDay", SaveType.WhiteNight },
 		};
-		private static readonly Dictionary<SaveType, IJsonParser> jsonParserMap = new Dictionary<SaveType, IJsonParser>()
+		private static readonly Dictionary<SaveType, JsonParserBase> jsonParserMap = new Dictionary<SaveType, JsonParserBase>()
 		{
 			{ SaveType.Settings, new SettingsJsonParser() },
+			{ SaveType.Etc, new EtcJsonParser() },
+			{ SaveType.Master, new MasterJsonParser() },
+			{ SaveType.Global, new GlobalJsonParser() },
+			{ SaveType.WhiteNight, new WhiteNightJsonParser() },
 		};
 		private static readonly Dictionary<SaveType, string> defaultBinaryFileNameMap = new Dictionary<SaveType, string>()
 		{
 			{ SaveType.Settings, "Lobotomy170808state.dat" },
+			{ SaveType.Etc, "etc170808.dat" },
+			{ SaveType.Master, "saveData170808.dat" },
+			{ SaveType.Global, "saveGlobal170808.dat" },
+			{ SaveType.WhiteNight, "100014.dat" },
 		};
 		private SaveType? _type;
 		private string _defaultBinaryFileName;
@@ -55,10 +67,6 @@ namespace LobCorp.Save
 			Global,
 			Unlimited,
 			WhiteNight,
-		}
-		private interface IJsonParser
-		{
-			bool TryParse(JObject save, out Dictionary<string, object> data);
 		}
 		public SaveType Type
 		{
@@ -113,25 +121,114 @@ namespace LobCorp.Save
 			}
 			throw new BadSaveFile("Unable to infer save type.");
 		}
-		private class SettingsJsonParser : IJsonParser
+		private abstract class JsonParserBase
 		{
-			public bool TryParse(JObject save, out Dictionary<string, object> data)
+			private readonly SaveType type;
+			public JsonParserBase(SaveType type)
 			{
-				if (InferType(save) != SaveType.Settings)
+				this.type = type;
+			}
+			public abstract bool TryParse(JObject save, out Dictionary<string, object> data);
+			protected bool CanParse(JObject save)
+			{
+				return InferType(save) == type;
+			}
+			protected void Copy<T>(Dictionary<string, object> dest, JObject src, string attr)
+			{
+				dest[attr] = src[attr].Value<T>();
+			}
+			protected void CopyList<T>(Dictionary<string, object> dest, JObject src, string attr)
+			{
+				dest[attr] = src[attr].Values<T>().ToList();
+			}
+		}
+		private class SettingsJsonParser : JsonParserBase
+		{
+			public SettingsJsonParser() : base(SaveType.Settings)
+			{
+			}
+			public override bool TryParse(JObject save, out Dictionary<string, object> data)
+			{
+				if (!CanParse(save))
 				{
 					data = null;
 					return false;
 				}
-				Dictionary<string, object> result = new Dictionary<string, object>();
+				data = new Dictionary<string, object>();
 
-				result["bgmVolume"] = save["bgmVolume"].Value<float>();
-				result["masterVolume"] = save["masterVolume"].Value<float>();
-				result["tooltip"] = save["tooltip"].Value<bool>();
-				result["language"] = save["language"].Value<string>();
-				result["logIndex"] = save["logIndex"].Value<int>();
+				Copy<float>(data, save, "bgmVolume");
+				Copy<float>(data, save, "bgmVolume");
+				Copy<float>(data, save, "masterVolume");
+				Copy<bool>(data, save, "tooltip");
+				Copy<string>(data, save, "language");
+				Copy<int>(data, save, "logIndex");
 
-				data = result;
 				return true;
+			}
+		}
+		private class EtcJsonParser : JsonParserBase
+		{
+			public EtcJsonParser() : base(SaveType.Etc)
+			{
+			}
+			public override bool TryParse(JObject save, out Dictionary<string, object> data)
+			{
+				if (!CanParse(save))
+				{
+					data = null;
+					return false;
+				}
+				data = new Dictionary<string, object>();
+
+				Copy<string>(data, save, "sefirabossTutorialPlayed");
+				Copy<bool>(data, save, "e0");
+				Copy<bool>(data, save, "e1");
+				Copy<bool>(data, save, "e2");
+				Copy<bool>(data, save, "e3");
+				Copy<bool>(data, save, "e4");
+				CopyList<long>(data, save, "waitingCreature");
+
+				return true;
+			}
+		}
+		private class MasterJsonParser : JsonParserBase
+		{
+			public MasterJsonParser() : base(SaveType.Master)
+			{
+			}
+			public override bool TryParse(JObject save, out Dictionary<string, object> data)
+			{
+				throw new NotImplementedException();
+			}
+		}
+		private class GlobalJsonParser : JsonParserBase
+		{
+			public GlobalJsonParser() : base(SaveType.Global)
+			{
+			}
+			public override bool TryParse(JObject save, out Dictionary<string, object> data)
+			{
+				throw new NotImplementedException();
+			}
+		}
+		private class UnlimitedJsonParser : JsonParserBase
+		{
+			public UnlimitedJsonParser() : base(SaveType.Unlimited)
+			{
+			}
+			public override bool TryParse(JObject save, out Dictionary<string, object> data)
+			{
+				throw new NotImplementedException();
+			}
+		}
+		private class WhiteNightJsonParser : JsonParserBase
+		{
+			public WhiteNightJsonParser() : base(SaveType.WhiteNight)
+			{
+			}
+			public override bool TryParse(JObject save, out Dictionary<string, object> data)
+			{
+				throw new NotImplementedException();
 			}
 		}
 	}
