7a71bd0e084ddce30bff63bd39572594d086fbf4
SOgz12Z3Ce <SOgz12Z3Ce@proton.me>
2026-09-03 17:28:37 +0800

    feat: add magic_parser

--- src/de.rs
+++ src/de.rs
@@ -1,18 +1,22 @@
 use crate::{error::Error, raw::File};
-use winnow::{
-    Parser,
-    error::{StrContext, StrContextValue},
-    token::literal,
-};
+use winnow::{Parser, binary, token};
 
 pub fn parse(input: Vec<u8>) -> Result<File, Error> {
-    Ok(parser.parse(&input)?)
+    let (file, _) = (parser, token::rest).parse(&input)?;
+    Ok(file)
 }
 
 pub fn parser(input: &mut &[u8]) -> winnow::Result<File> {
-    literal(0x00)
-        .context(StrContext::Label("magic number"))
-        .context(StrContext::Expected(StrContextValue::Description("magic")))
-        .parse_next(input)?;
+    magic_parser.parse_next(input)?;
     Ok(File { objects: vec![] })
+}
+
+fn magic_parser(input: &mut &[u8]) -> winnow::Result<()> {
+    token::literal(0x00).parse_next(input)?;
+    binary::le_i32.verify(|i| *i == 1).parse_next(input)?;
+    binary::le_i32.verify(|i| *i == -1).parse_next(input)?;
+    binary::le_i32.verify(|i| *i == 1).parse_next(input)?;
+    binary::le_i32.verify(|i| *i == 0).parse_next(input)?;
+
+    Ok(())
 }
